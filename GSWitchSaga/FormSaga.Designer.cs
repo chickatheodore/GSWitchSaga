37f014cd2f69a6345f02381167e9001e2ff53d1f
Mirana <mmirana@yahoo.com>
2021-01-21 07:13:55 +0800

    Initial commit & push for Geekseat Witch Saga assessment test.

---
+++ GSWitchSaga/FormSaga.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace GSWitchSaga
+{
+    partial class FormSaga
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textPerson_A_YOD = new System.Windows.Forms.TextBox();
+            this.label_A_YOD = new System.Windows.Forms.Label();
+            this.textPerson_A_AOD = new System.Windows.Forms.TextBox();
+            this.label_A_AOD = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textPerson_B_YOD = new System.Windows.Forms.TextBox();
+            this.label_B_YOD = new System.Windows.Forms.Label();
+            this.textPerson_B_AOD = new System.Windows.Forms.TextBox();
+            this.label_B_AOD = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textPerson_A_YOD);
+            this.groupBox1.Controls.Add(this.label_A_YOD);
+            this.groupBox1.Controls.Add(this.textPerson_A_AOD);
+            this.groupBox1.Controls.Add(this.label_A_AOD);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 68);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Person A ";
+            // 
+            // textPerson_A_YOD
+            // 
+            this.textPerson_A_YOD.Location = new System.Drawing.Point(283, 29);
+            this.textPerson_A_YOD.Name = "textPerson_A_YOD";
+            this.textPerson_A_YOD.Size = new System.Drawing.Size(50, 20);
+            this.textPerson_A_YOD.TabIndex = 3;
+            // 
+            // label_A_YOD
+            // 
+            this.label_A_YOD.AutoSize = true;
+            this.label_A_YOD.Location = new System.Drawing.Point(201, 32);
+            this.label_A_YOD.Name = "label_A_YOD";
+            this.label_A_YOD.Size = new System.Drawing.Size(79, 13);
+            this.label_A_YOD.TabIndex = 2;
+            this.label_A_YOD.Text = "Year of Death :";
+            // 
+            // textPerson_A_AOD
+            // 
+            this.textPerson_A_AOD.Location = new System.Drawing.Point(120, 29);
+            this.textPerson_A_AOD.Name = "textPerson_A_AOD";
+            this.textPerson_A_AOD.Size = new System.Drawing.Size(50, 20);
+            this.textPerson_A_AOD.TabIndex = 1;
+            // 
+            // label_A_AOD
+            // 
+            this.label_A_AOD.AutoSize = true;
+            this.label_A_AOD.Location = new System.Drawing.Point(38, 32);
+            this.label_A_AOD.Name = "label_A_AOD";
+            this.label_A_AOD.Size = new System.Drawing.Size(76, 13);
+            this.label_A_AOD.TabIndex = 0;
+            this.label_A_AOD.Text = "Age of Death :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textPerson_B_YOD);
+            this.groupBox2.Controls.Add(this.label_B_YOD);
+            this.groupBox2.Controls.Add(this.textPerson_B_AOD);
+            this.groupBox2.Controls.Add(this.label_B_AOD);
+            this.groupBox2.Location = new System.Drawing.Point(12, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(415, 68);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " Person B ";
+            // 
+            // textPerson_B_YOD
+            // 
+            this.textPerson_B_YOD.Location = new System.Drawing.Point(283, 29);
+            this.textPerson_B_YOD.Name = "textPerson_B_YOD";
+            this.textPerson_B_YOD.Size = new System.Drawing.Size(50, 20);
+            this.textPerson_B_YOD.TabIndex = 3;
+            // 
+            // label_B_YOD
+            // 
+            this.label_B_YOD.AutoSize = true;
+            this.label_B_YOD.Location = new System.Drawing.Point(201, 32);
+            this.label_B_YOD.Name = "label_B_YOD";
+            this.label_B_YOD.Size = new System.Drawing.Size(79, 13);
+            this.label_B_YOD.TabIndex = 2;
+            this.label_B_YOD.Text = "Year of Death :";
+            // 
+            // textPerson_B_AOD
+            // 
+            this.textPerson_B_AOD.Location = new System.Drawing.Point(120, 29);
+            this.textPerson_B_AOD.Name = "textPerson_B_AOD";
+            this.textPerson_B_AOD.Size = new System.Drawing.Size(50, 20);
+            this.textPerson_B_AOD.TabIndex = 1;
+            // 
+            // label_B_AOD
+            // 
+            this.label_B_AOD.AutoSize = true;
+            this.label_B_AOD.Location = new System.Drawing.Point(38, 32);
+            this.label_B_AOD.Name = "label_B_AOD";
+            this.label_B_AOD.Size = new System.Drawing.Size(76, 13);
+            this.label_B_AOD.TabIndex = 0;
+            this.label_B_AOD.Text = "Age of Death :";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textResult);
+            this.groupBox3.Location = new System.Drawing.Point(12, 214);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(415, 132);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(6, 19);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ReadOnly = true;
+            this.textResult.Size = new System.Drawing.Size(403, 107);
+            this.textResult.TabIndex = 0;
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Location = new System.Drawing.Point(188, 176);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(75, 32);
+            this.buttonSolve.TabIndex = 2;
+            this.buttonSolve.Text = "Solve";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.buttonSolve_Click);
+            // 
+            // FormSaga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 360);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSaga";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geekseat Witch Saga";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textPerson_A_YOD;
+        private System.Windows.Forms.Label label_A_YOD;
+        private System.Windows.Forms.TextBox textPerson_A_AOD;
+        private System.Windows.Forms.Label label_A_AOD;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textPerson_B_YOD;
+        private System.Windows.Forms.Label label_B_YOD;
+        private System.Windows.Forms.TextBox textPerson_B_AOD;
+        private System.Windows.Forms.Label label_B_AOD;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button buttonSolve;
+    }
+}
+
---
+++ GSWitchSaga/FormSaga.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GSWitchSaga
+{
+    public partial class FormSaga : Form
+    {
+        public FormSaga()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            //Clearing result textbox
+            textResult.Text = "";
+
+            //Make sure all parameters for Person A are valid.
+            bool validPersonA = ValidInput(textPerson_A_AOD) && ValidInput(textPerson_A_YOD);       //data validation for Person A
+            bool validPersonB = ValidInput(textPerson_B_AOD) && ValidInput(textPerson_B_YOD);       //data validation for Person B
+
+            if (!validPersonA || !validPersonB)                                                     //make sure all inputs are valid
+                return;
+
+            //Declaring variables
+            int person_A_AgeOfDeath = Convert.ToInt32(textPerson_A_AOD.Text);
+            int person_A_YearOfDeath = Convert.ToInt32(textPerson_A_YOD.Text);
+
+            int person_B_AgeOfDeath = Convert.ToInt32(textPerson_B_AOD.Text);
+            int person_B_YearOfDeath = Convert.ToInt32(textPerson_B_YOD.Text);
+
+            //Find year series based on given age of death and year of death
+            int person_A_Year_Series = person_A_YearOfDeath - person_A_AgeOfDeath;
+            int person_B_Year_Series = person_B_YearOfDeath - person_B_AgeOfDeath;
+
+            //Find the number of people killed by specific year series
+            int person_A_NumberOfKilled = NumberOfPeopleKilled(person_A_Year_Series);
+            int person_B_NumberOfKilled = NumberOfPeopleKilled(person_B_Year_Series);
+
+
+            //Creating output to result textbox
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendFormat("Person A born on Year = {0} – {1} = {2}", person_A_YearOfDeath, person_A_AgeOfDeath, person_A_Year_Series);
+
+            if (person_A_NumberOfKilled < 0)
+                builder.Append(", invalid data given.\r\n");
+            else
+                builder.AppendFormat(", number of people killed on year {0} is {1}.\r\n", person_A_Year_Series, person_A_NumberOfKilled);
+
+
+            builder.AppendFormat("Person B born on Year = {0} – {1} = {2}", person_B_YearOfDeath, person_B_AgeOfDeath, person_B_Year_Series);
+
+            if (person_B_NumberOfKilled < 0)
+                builder.Append(", invalid data given.\r\n");
+            else
+                builder.AppendFormat(", number of people killed on year {0} is {1}.\r\n", person_B_Year_Series, person_B_NumberOfKilled);
+
+
+            //Check if you give invalid data
+            if (person_A_NumberOfKilled < 0 || person_B_NumberOfKilled < 0)
+            {
+                builder.AppendFormat("\r\nReturn -1. Invalid data.");
+                textResult.Text = builder.ToString();
+
+                MessageBox.Show("Return -1. Invalid data", "Invalid data");
+            }
+            else
+            {
+                //Find the average of people killed
+                decimal average = ((decimal)(person_A_NumberOfKilled + person_B_NumberOfKilled) / 2);
+
+                builder.AppendFormat("\r\nSo the average is ( {0} + {1} )/2 = {2}",
+                    person_A_NumberOfKilled, person_B_NumberOfKilled, average);
+
+                textResult.Text = builder.ToString();
+            }
+        }
+
+
+        /*
+         * Validation for TextBox's text.
+         * Check for empty text and then validating the text is number
+         * 
+         * @param textBox (TextBox), the TextBox Control to be validate.
+         * return type boolean.
+        */
+
+        private bool ValidInput(TextBox textBox)
+        {
+            //Clean the input from whitespace
+            string text = textBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(text))
+            {
+                //if the value is empty then return not valid
+                textBox.Focus();
+                return false;
+            }
+            else
+            {
+                bool isNumber = text.All(char.IsNumber);    //check if input is in numeric format
+                if (!isNumber)
+                {
+                    textBox.Focus();
+                    textBox.SelectAll();
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        /*
+         * Find the number of villager(s) the witch has to kill on specified year.
+         * The pattern is Fibonacci series, using Iterative approach.
+         * 
+         * @param year (int), year series.
+         * return type int.
+        */
+
+        private int NumberOfPeopleKilled(int year)
+        {
+            //Skip operation and return the parameter's value
+            if (year <= 0) return year;
+
+            List<int> values = new List<int>() { 0, 1 };
+
+            int firstNumber = 0, secondNumber = 1, nextNumber = 0;
+
+            //return zero if year series less than or equal to zero
+            if (year <= 0)
+                return firstNumber;
+
+            for (int i = 2; i <= year; i++)
+            {
+                nextNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+                values.Add(secondNumber);
+            }
+
+            int result = 0;
+            foreach (int value in values)
+                result += value;
+
+            return result;
+        }
+
+
+        /*
+         * Some people loves to press Enter (replacing Tab) to switch between controls.
+        */
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            Type type = this.ActiveControl.GetType();
+            bool allowTab = !type.Equals(typeof(Button));
+
+            if (keyData == (Keys.Enter) && allowTab)
+            {
+                SendKeys.Send("{TAB}");
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+    }
+}
